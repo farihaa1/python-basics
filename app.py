0c49bf98a5b5ffd2aaca729e6106117c2cdb6972
farihaa1 <farihaa1403@gmail.com>
2026-08-27 22:30:53 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+age = 20
+name = "rahim"
+height = 5.8
+is_Learning = True
+
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(is_Learning))
+
+# basic math
+a = 10
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(10//3)  # floor division 3
+print(10 % 3)  # remainder 1
+print(10 ** 3)  # power 1000
+
+#stings combine
+first_name = "fariha"
+last_name = "sarkar"
+
+full_name = first_name+ " "+last_name
+print(full_name)
+
+print(f"my name is {name} and i am {age} years old")
+
+#user input
+
+name = input("what is your name? \n")
+print(f"hello,{name}")
+
